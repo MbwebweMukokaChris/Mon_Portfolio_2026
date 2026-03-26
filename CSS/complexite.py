ab4a78d8de2bfc6e747b8291d1ca1767cdb144b6
7777-chris <24mm245si@udbl.ac.cd>
2026-03-26 16:11:54 +0200

    stylisation de la section a propos

---
+++ CSS/complexite.py
@@ -0,0 +1,22 @@
+   " def calculer_somme(nombres):"
+        "somme=0"
+        "compteur_ops=0"
+        "for nombre in nombres:
+            "somme+=nombre
+           "" compteur_ops+=1
+        "return somme,compteur_ops
+
+   "" somme,nb_ops=calculer_somme([4,5,76,88,3])
+    "print(somme)
+    "print(nb_ops)
+
+
+    def deux_nombres(liste):
+        bn=[]
+        for j in list:
+           for i in list:
+              if j in (len(list))==i:
+                 nb+=1
+        return nb
+c=deux_nombres([1,2,3,4,6])
+print(deux_nombres)
